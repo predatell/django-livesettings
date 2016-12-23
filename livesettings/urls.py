90eaafd9deeb93954d9556c9ed5c3350c8bd55eb
predatell <seryi_belyi@inbox.ru>
2016-12-23 16:18:08 +0000

    patterns is not available in django.conf.urls of version 1.10

--- livesettings/urls.py
+++ livesettings/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import patterns, url
+from . import views
 
-urlpatterns = patterns('livesettings.views',
-    url(r'^$', 'site_settings', name='satchmo_site_settings'),
-    url(r'^export/$', 'export_as_python', name='settings_export'),
-    url(r'^(?P<group>[^/]+)/$', 'group_settings', name='livesettings_group'),
-)
+urlpatterns = [
+    (r'^$', views.site_settings, {}, 'satchmo_site_settings'),
+    (r'^export/$', views.export_as_python, {}, 'settings_export'),
+    (r'^(?P<group>[^/]+)/$', views.group_settings),
+]
