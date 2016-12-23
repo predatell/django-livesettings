6bef0c51115ca9698a69eb62cbb196f8ce75385b
predatell <seryi_belyi@inbox.ru>
2016-12-23 20:31:05 +0000

    urls.py edited online with Bitbucket

--- livesettings/urls.py
+++ livesettings/urls.py
@@ -1,7 +1,9 @@
+from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
-    (r'^$', views.site_settings, {}, 'satchmo_site_settings'),
-    (r'^export/$', views.export_as_python, {}, 'settings_export'),
-    (r'^(?P<group>[^/]+)/$', views.group_settings),
+    url(r'^$', views.site_settings, {}, 'satchmo_site_settings'),
+    url(r'^export/$', views.export_as_python, {}, 'settings_export'),
+    url(r'^(?P<group>[^/]+)/$', views.group_settings),
 ]
